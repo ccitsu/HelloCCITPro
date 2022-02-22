5be32b7d51d5bec8f3d59e5d05ceb2b6a278e36e
Shaqra University <ccitlms@gmail.com>
2022-02-22 17:47:21 +0300

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by CCIT on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
